d2cc662b3cb81893e146fd471abb0f97d7229bf4
Hamidreza Dadafarid <148267814+HamidrezaDadafarid@users.noreply.github.com>
2025-07-20 23:45:05 +0330

    added question numeber 197

---
+++ #197/Solution.sql
@@ -0,0 +1,10 @@
+Create table If Not Exists Weather (id int, recordDate date, temperature int)
+Truncate table Weather
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10')
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25')
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
+
+SELECT w2.id
+FROM Weather AS w1, Weather AS w2
+WHERE w1.recorddate = w2.recorddate - 1 AND w1.temperature < w2.temperature
